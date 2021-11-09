ca24b293cc609cfab01c03b8fafb8baacf8d418f
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-08 20:53:48 -0500

    Saved the short and long key value pair to database

--- express_server.js
+++ express_server.js
@@ -46,8 +46,11 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  console.log(urlDatabase);  // Log the POST request body to the console
+  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/hello", (req, res) => {

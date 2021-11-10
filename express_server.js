f1fadf1eecbb38b8ee855095df9b72422e02896d
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-09 22:30:01 -0500

    Added endpoint to POST to /login Express server

--- express_server.js
+++ express_server.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const cookieParser = require("cookie-parser")
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 
 app.set("view engine", "ejs");
-
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -69,6 +70,11 @@ app.post('/urls/:shortURL', (req, res)=> {
   urlDatabase[shortURL] = longURL;
   res.redirect('/urls');
 });
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+  })
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");

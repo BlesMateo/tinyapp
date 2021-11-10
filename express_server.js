878eea98d8dcc501f14d749d7b335531900380b5
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-10 09:32:54 -0500

    Added cookies to render in express server  and added login and logout functionality to header.ejs

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser");
+const { request } = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -33,16 +34,26 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+  urls: urlDatabase,
+  username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+  username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+  shortURL: req.params.shortURL,
+  longURL: urlDatabase[req.params.shortURL],
+  username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -72,9 +83,15 @@ app.post('/urls/:shortURL', (req, res)=> {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+  let username = req.body.username
+  res.cookie("username", username);
   res.redirect("/urls");
   })
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect(`/urls`);
+  });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
